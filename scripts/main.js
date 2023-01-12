d79036e8fe54010c6f75df9cf7271375eb26bd0c
xiaonanpols21 <xiaonanpolspols@gmail.com>
2023-01-12 21:13:43 +0100

    verder gaan met lelijke like knop

--- scripts/main.js
+++ scripts/main.js
@@ -8,18 +8,18 @@ Bron: https://benfrain.com/automatically-play-and-pause-video-as-it-enters-and-l
 Play video's when video is on screen. Volume doesn't work
 */
 
+
+// Poging 1
+// Like button
+// Bron: https://codepen.io/xiao-nan-pols/pen/mdKZwoj
+// If button clicked, all like btns get pink. Iets doen met de clicked button
 /*
-Poging 1
-Like button
-Bron: https://codepen.io/xiao-nan-pols/pen/mdKZwoj
-If button clicked, all like btns get pink. Iets doen met de clicked button
 const likeBtns = document.querySelectorAll("#like");
-const dislikeBtns = document.querySelectorAll("#dislike");
 
 function likeActive(buttonPressed, whichButton) {
-    let allLikeBtns = document.querySelectorAll("#like");
+    //let allLikeBtns = document.querySelectorAll("#like");
 
-    allLikeBtns.forEach(item => {
+    likeBtns.forEach(item => {
         item.classList.add("likeActive");
     });
 };
@@ -28,8 +28,9 @@ likeBtns.forEach(button => {
     button.addEventListener("click", likeActive);
 });
 */
+
+//Poging 2
 /*
-Poging 2
 const likeBtns = document.querySelectorAll("#like");
 const dislikeBtns = document.querySelectorAll("#dislike");
 
@@ -49,16 +50,16 @@ function likeActive(buttonPressed, whichButton) {
 };
 
 likeBtns.forEach(button => {
-    button.addEventListener("click", likeActive(true, e.target.value));
+    //button.addEventListener("click", likeActive(true, e.target.value));
+    button.forEach(e => {
+        likeActive(true, e.target.value);
+    });
 });
 */
 
-
-/*
-Lelijke versie die je 4x moet herhalen
+// Lelijke versie die je 4x moet herhalen
 const like1 = document.querySelector("#like-1");
 function likeActive() {
     like1.classList.add("likeActive");
 };
 like1.addEventListener("click", likeActive);
-*/
