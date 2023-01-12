b83b38e798f92a2236fa1a7ba66349602401b237
xiaonanpols21 <xiaonanpolspols@gmail.com>
2023-01-12 20:58:35 +0100

    poging 2 like button

--- scripts/main.js
+++ scripts/main.js
@@ -9,13 +9,14 @@ Play video's when video is on screen. Volume doesn't work
 */
 
 /*
+Poging 1
 Like button
 Bron: https://codepen.io/xiao-nan-pols/pen/mdKZwoj
 If button clicked, all like btns get pink. Iets doen met de clicked button
 const likeBtns = document.querySelectorAll("#like");
 const dislikeBtns = document.querySelectorAll("#dislike");
 
-function likeActive() {
+function likeActive(buttonPressed, whichButton) {
     let allLikeBtns = document.querySelectorAll("#like");
 
     allLikeBtns.forEach(item => {
@@ -27,11 +28,37 @@ likeBtns.forEach(button => {
     button.addEventListener("click", likeActive);
 });
 */
+/*
+Poging 2
+const likeBtns = document.querySelectorAll("#like");
+const dislikeBtns = document.querySelectorAll("#dislike");
 
+function likeActive(buttonPressed, whichButton) {
+    //let allLikeBtns = document.querySelectorAll("#like");
+
+    likeBtns.forEach(item => {
+        //item.classList.add("likeActive");
+
+        if(buttonPressed) {
+            if(whichButton == 1) {
+                item.classList.add("likeActive");
+            }
+        }
+
+    });
+};
+
+likeBtns.forEach(button => {
+    button.addEventListener("click", likeActive(true, e.target.value));
+});
+*/
+
+
+/*
+Lelijke versie die je 4x moet herhalen
 const like1 = document.querySelector("#like-1");
-
 function likeActive() {
     like1.classList.add("likeActive");
 };
-
 like1.addEventListener("click", likeActive);
+*/
