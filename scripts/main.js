ccba7e61df7c61b61a2cc924c67007bc12e0a36b
xiaonanpols21 <xiaonanpolspols@gmail.com>
2023-01-12 20:46:02 +0100

    poging 2 like buttons

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,27 @@
 import '../styles/style.css'
 import * as d3 from 'd3';
+import { interpolateMagma } from 'd3';
 
 /*
 Bron: https://benfrain.com/automatically-play-and-pause-video-as-it-enters-and-leaves-the-viewport-screen/
 Play video's when video is on screen. Volume doesn't work
 */
 
+/*
+Bron: https://codepen.io/xiao-nan-pols/pen/mdKZwoj
+If button clicked, all like btns get pink
+const likeBtns = document.querySelectorAll("#like");
+const dislikeBtns = document.querySelectorAll("#dislike");
+
+function likeActive() {
+    let allLikeBtns = document.querySelectorAll("#like");
+
+    allLikeBtns.forEach(item => {
+        item.classList.add("likeActive");
+    });
+};
+
+likeBtns.forEach(button => {
+    button.addEventListener("click", likeActive);
+});
+*/
