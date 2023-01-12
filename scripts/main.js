0de77c67ecb4ac248c144de6319d2c7d8bb1a78d
xiaonanpols21 <xiaonanpolspols@gmail.com>
2023-01-12 20:27:17 +0100

    aan de gang met tiktok

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,8 @@
 import '../styles/style.css'
 import * as d3 from 'd3';
+
+/*
+Bron: https://benfrain.com/automatically-play-and-pause-video-as-it-enters-and-leaves-the-viewport-screen/
+Play video's when video is on screen. Volume doesn't work
+*/
+
