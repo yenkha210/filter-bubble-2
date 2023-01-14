f585951d21fea0558adfa676e95902d77cd6ade9
xiaonanpols21 <xiaonanpolspols@gmail.com>
2023-01-14 18:53:55 +0100

    duimpjes stijlen en popup

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,15 @@ Play vid if vid is on screen
 Bron: https://benfrain.com/automatically-play-and-pause-video-as-it-enters-and-leaves-the-viewport-screen/
 Play video's when video is on screen. Volume doesn't work
 */
+
+// Variables
+const chooseGereedBtn = document.querySelector(".tiktok-vid button");
+const duikPopUp = document.querySelector(".duik-pop-up");
+
+// Duik Pop Up
+duikPopUp.classList.add("none");
+
+function showDuikPopUp() {
+    duikPopUp.classList.remove("none");
+}
+chooseGereedBtn.addEventListener("click", showDuikPopUp);
